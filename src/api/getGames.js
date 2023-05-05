c24d644fb32182863c531ce62de99690c6ba165f
Lucek1945 <kam.kornobis@gmail.com>
2023-05-05 14:35:59 +0200

    a

--- src/api/getGames.js
+++ src/api/getGames.js
@@ -4,7 +4,7 @@ export const getGames = () => {
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': '6595002667msh58518e4e7ab6d3bp11f99bjsn8e18e855b2db',
+            
             'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
         }
     };
--- src/api/getMovies.js
+++ src/api/getMovies.js
@@ -3,7 +3,6 @@ export const getMovies = () => {
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': 'b320860d0fmsh62304f3bccb359bp107198jsn9f636ca1706e',
             'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
         }
     };
